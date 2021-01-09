e0a5a3905bb482c29157e223160a0f1d7866064a
qt_coder <65493032+qt-coder@users.noreply.github.com>
2021-01-09 16:47:11 +0800

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1><strong>qtAnime</strong>DB</h1>
+        </header>
+    )
+}
+
+export default Header
